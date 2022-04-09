ba030e7d735851710db2a8afdafc5039f7681b60
gemuz_work <gemuzkm@gmail.com>
2022-04-09 12:56:13 +0300

    add controller

---
+++ src/main/java/com/example/springwebsecurityc51/controller/CalcController.java
@@ -0,0 +1,20 @@
+package com.example.springwebsecurityc51.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class CalcController {
+
+    @GetMapping
+    public String calc() {
+        return "calc";
+    }
+
+    @PostMapping
+    public String calc(double a, double b, String op, Model model) {
+        double result = a + b;
+        model.addAttribute("result", result);
+        return "calc";
+    }
+}
---
+++ src/main/java/com/example/springwebsecurityc51/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.springwebsecurityc51.controller;
+
+import com.example.springwebsecurityc51.entity.User;
+import com.example.springwebsecurityc51.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/reg")
+    public String reg() {
+        return "reg";
+    }
+
+    @PostMapping("/reg")
+    public String reg(User user) {
+        userService.save(user);
+        return "reg";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+}
