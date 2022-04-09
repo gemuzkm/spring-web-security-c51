25298346dce1c911a4854231978be867df22aaf1
gemuz_work <gemuzkm@gmail.com>
2022-04-09 09:55:41 +0300

    Initial commit

---
+++ src/main/java/com/example/springwebsecurityc51/SpringWebSecurityC51Application.java
@@ -0,0 +1,13 @@
+package com.example.springwebsecurityc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebSecurityC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebSecurityC51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebsecurityc51/SpringWebSecurityC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebsecurityc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebSecurityC51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
