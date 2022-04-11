dd53e7be9009cafc3384413e89e01f52bd1b5d66
gemuz_work <gemuzkm@gmail.com>
2022-04-11 09:47:27 +0300

    test working

--- src/main/java/com/example/springwebsecurityc51/controller/CalcController.java
+++ src/main/java/com/example/springwebsecurityc51/controller/CalcController.java
@@ -1,9 +1,13 @@
 package com.example.springwebsecurityc51.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/calc")
 public class CalcController {
 
     @GetMapping
--- src/main/java/com/example/springwebsecurityc51/controller/UserController.java
+++ src/main/java/com/example/springwebsecurityc51/controller/UserController.java
@@ -3,9 +3,13 @@ package com.example.springwebsecurityc51.controller;
 import com.example.springwebsecurityc51.entity.User;
 import com.example.springwebsecurityc51.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/user")
 public class UserController {
 
     @Autowired
